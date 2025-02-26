b9c1aebd4748fbc23000a999f4ac2ea93e1f616a
Khristopher <khriss201403@gmail.com>
2025-02-26 14:45:50 -0500

    Commmit numero 1 actualizado y corregido y enviado nuevo archivo.

---
+++ modificando_commit.py
@@ -0,0 +1,3 @@
+# Este es un simple archivo que no contendrá código, solo se usará para probar el modificar commits.
+
+# Este es el primer commit enviado.
---
+++ nuevo.py
@@ -0,0 +1,3 @@
+# Este archivo se agregará al commit, para comprobar que se puede
+# agregar un archivo al modificar un commit.
+
