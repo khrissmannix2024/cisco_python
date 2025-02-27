e6c23386d5c064616cbf92f210ff8791d697e522
Khristopher <khriss201403@gmail.com>
2025-02-27 08:49:57 -0500

    Eliminé dos archivos de prueba, con los que realicé ejercicios de modificación de commit

--- modificando_commit.py
+++
@@ -1,3 +0,0 @@
-# Este es un simple archivo que no contendrá código, solo se usará para probar el modificar commits.
-
-# Este es el primer commit enviado.
--- nuevo.py
+++
@@ -1,3 +0,0 @@
-# Este archivo se agregará al commit, para comprobar que se puede
-# agregar un archivo al modificar un commit.
-
